49c0bd5aa19888ea7cf04563cc22197c63b56d34
Mxolisi Dladla <mxohdapage@gmail.com>
2020-07-04 21:24:36 +0200

    master

---
+++ src/rover.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Rover extends React.Component {
+    render() {
+        const { facing, ghost } = this.props;
+        return <span className={`rover ${facing} ${ghost ? 'ghost' : ''} `}>🛦</span>
+    }
+};
+
+export default Rover;
