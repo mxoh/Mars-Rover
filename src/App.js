88466f816fa1279d5e874d05b21d2030726bc044
Mxolisi Dladla <mxohdapage@gmail.com>
2020-07-05 18:10:37 +0200

    master

--- src/App.js
+++ src/App.js
@@ -1,134 +1,120 @@
-import React from 'react';
-//import {render} from 'react-dom';
+import React from "react";
+//import {render} from "react-dom";
 import "./index.css";
 import Mars from "./mars";
 
+class App1 extends React.Component {
 
-class App extends React.Component {
-
-  state = {
-    commands: '',
-    commandsToExecute: '',
-    execute: '',
-    startPosition: '00N'
-  };
-
-  addCommand = e => {
-    this.setState(
-      {
-        commands: this.setState.commands + e.target.value
-      }
-    )
-  };
-
-  runSample = e => {
-    this.setState(
-      {
-        commands: e.target.value
-      }
-    );
-  };
-
-  execute = () => {
-    let startPosition = this.startInput.value;
-    if (/^[0-4][0-4][NEWS]$/.test(startPosition)) {
-      this.setState(
-        {
-          execute: true,
-          commandsToExecute: this.state.commands,
-          startPosition
-        }
-      );
-    } else {
-      alert('Invalid start position')
-    }
-  }
-
-  clear = () => {
-    this.setState(
-      {
+    state = {
         commands: '',
+        commandsToExecute: '',
         execute: false,
-        commandsToExecute: ''
-      }
-    );
-  };
+        startPosition: '00N'
+    };
 
-  validateStartPosition = e => {
-    e.target.checkValidator();
-  };
+    addCommand = (e) => {
+        this.setState({
+            commands: this.state.commands + e.target.value
+        })
+    };
 
-  stopExecute = () => {
-    this.setState(
-      {
-        execute: false
-      }
-    );
-  };
+    runSample = (e) => {
+        this.setState({
+            commands: e.target.value
+        });
+    };
 
-  render() {
-    let position = this.state.startPosition || '00N';
-    position = position.split('').join(' ');
-//<a className={'source'} href={''} title={'Source code for Mars Rover'}>Source</a>
-    return (
-      <div className={'app'}>
+    execute = () => {
+        let startPosition = this.startInput.value;
+        if (/^[0-4][0-4][NEWS]$/.test(startPosition)) {
+            this.setState({
+                execute: true,
+                commandsToExecute: this.state.commands,
+                startPosition
+            });
+        } else {
+            alert('Invalid start position.');
+        }
 
-        <h1 className={'app-name'}>Mars Rover</h1>
-        
+    };
 
-        <div className={`control-panel`}>
+    clear = () => {
+        this.setState({
+            commands: '',
+            execute: false,
+            commandsToExecute: ''
+        });
+    };
 
-          <div className='start-position'>
-            <label htmlFor="startPosition">Start Position (Eg; 00N):</label>
-            <input type="text" 
-                    id="startPosition" 
-                    maxLength={3} required
-                    pattern={'^[0-4][0-4][NEWS]$'}
-                    defaultValue={'00N'} 
-                    onBlur={this.validateStartPosition}
-                    ref={(elm) => {
-                      this.startInput = elm
-                    }}
-            />
-          </div>
+    validateStartPosition = (e) => {
+        e.target.checkValidity();
+    };
 
-          <div className='commands'>
-            <button value='M' onClick={this.addCommand}>Move</button>
-            <button value='L' onClick={this.addCommand}>Left</button>
-            <button value='R' onClick={this.addCommand}>Right</button>
-          </div>
-          
-          <div className='execution'>
-            <button onClick={this.clear} className='secondary'>X</button>
-            <input type='text' readOnly value={this.state.commands} />
-            <button className={'cta'} onClick={this.execute}>Execute</button>
-          </div>
+    stopExecute = () => {
+        this.setState({
+            execute: false
+        });
+    };
 
-          <div className='Sample'>
-            <label>Sample: </label>
-            <ul>
-              <li>
-                <button value={'MMRMMLMMRM'} onClick={this.runSample}>MMRMMLMMRM</button>
-              </li>
-              <li>
-                <button value={'RMMMLMRMLM'} onClick={this.runSample}>RMMMLMRMLM</button>
-              </li>
-            </ul>
-          </div>
-          
-          <Mars 
-            size={5}
-            position={position}
-            commands={this.state.commandsToExecute}
-            execute={this.state.execute}
-            onDone={this.stopExecute}
-          />
-
-        </div>
-
-      </div>
-    )
-  }  
+    render() {
+        let position = this.state.startPosition || '00N';
+        position = position.split('').join(' ');
+        return (
+            <div className={'app'}>
+                <h1 className={'app-name'}>Mars Rover in JavaScript / React</h1>
+                <a className={'source'} href={'https://github.com/vraa/marsrover'}
+                   title={'Source code for Mars Rover in JavaScript / React'}>Source</a>
+                <div className={`control-panel`}>
+                    <div className={'start-position'}>
+                        <label
+                            htmlFor="startPosition"
+                        >
+                            Start Position (Eg; 00N):
+                        </label>
+                        <input type="text"
+                               id="startPosition"
+                               maxLength={3}
+                               required
+                               pattern={'^[0-4][0-4][NEWS]$'}
+                               defaultValue={'00N'}
+                               onBlur={this.validateStartPosition}
+                               ref={(elm) => {
+                                   this.startInput = elm
+                               }}
+                        />
+                    </div>
+                    <div className='commands'>
+                        <button value='M' onClick={this.addCommand}>Move</button>
+                        <button value='L' onClick={this.addCommand}>Left</button>
+                        <button value='R' onClick={this.addCommand}>Right</button>
+                    </div>
+                    <div className='execution'>
+                        <button onClick={this.clear} className='secondary'>✖</button>
+                        <input type="text" readOnly value={this.state.commands}/>
+                        <button className={'cta'} onClick={this.execute}>Execute</button>
+                    </div>
+                    <div className='samples'>
+                        <label>Sample: </label>
+                        <ul>
+                            <li>
+                                <button value={'MMRMMLMMRM'} onClick={this.runSample}>MMRMMLMMRM</button>
+                            </li>
+                            <li>
+                                <button value={'RMMMLMRMLM'} onClick={this.runSample}>RMMMLMRMLM</button>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <Mars
+                    size={5}
+                    position={position}
+                    commands={this.state.commandsToExecute}
+                    execute={this.state.execute}
+                    onDone={this.stopExecute}
+                />
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App1;
